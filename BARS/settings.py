ceb0074deab6b9f2862626b67f94f4da09231a98
shaikat020 <tahsin.azad.skt@gmail.com>
2026-04-06 12:42:21 +0600

    Refractor: modify settings.py

--- BARS/settings.py
+++ BARS/settings.py
@@ -135,7 +135,7 @@ LOGOUT_REDIRECT_URL = "index"
 
 # Email Configuration for Gmail SMTP
 # --- EMAIL UPLINK PROTOCOL ---
-EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="")
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
@@ -143,7 +143,7 @@ EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = config(
     "DEFAULT_FROM_EMAIL",
-    default="BAIUST-Robotics-Society-BARS <bars8thpanel@gmail.com>",
+    default="",
 )
 
 # Optional: If you want to see errors in the console during testing
